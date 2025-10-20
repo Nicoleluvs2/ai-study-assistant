0917cb39752cf58a00831f1912eac2fb5aaa6ae8
Nicoleluvs2 <146922448+Nicoleluvs2@users.noreply.github.com>
2025-10-20 10:00:30 +0100

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Theme toggle
+const toggleBtn = document.querySelector('.toggle-theme');
+toggleBtn?.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    if(localStorage.getItem('theme') === 'dark') document.body.classList.add('dark-mode');
+});
